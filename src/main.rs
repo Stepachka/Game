9e21e9ac3ef80c47cad515a23b4a2ce0322c590b
Stepan <stepan.talabaev@mail.ru>
2022-06-30 00:40:39 +0300

    initialized variables, loops

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
-//connected library
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
+    println!("Guess the number!");
+
     //init secret_number
+    let secret_number;
+    //input secret_number
+    
     //cycle with guess the numbers
-        //init guss number
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess;
+        //input guss number
+
+        println!("secrer number - {secret_number}, guess number - {guess")
         //hints
         //loop exit and output
+    }
+
+
 }
